cc59a14fe551a3e3b6526d1855bd0d5455aa3fc1
ssitko <ssitko@pgs-soft.com>
2018-05-14 15:08:39 +0200

    Removed dev temporary files

--- _tmp/constants/errors.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var INVALID_METHOD = exports.INVALID_METHOD = { error: 'This method is not allowed under requested endpoint' };
